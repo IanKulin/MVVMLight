ec237f75f6bb630904c9cb0fa8c02314dc156919
Ian Bailey <IanKulin@kulin.com.au>
2022-08-06 12:13:39 +0800

    Initial

---
+++ MVVMLight/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVMLight
+//
+//  Created by Ian Bailey on 6/8/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVMLight/MVVMLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMLightApp.swift
+//  MVVMLight
+//
+//  Created by Ian Bailey on 6/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct MVVMLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
